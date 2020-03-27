480af058e5d0559ffc23d00d1a255fc9cc5ae94d
Quim Broquetas <qbroquetas@outlook.com>
2020-03-27 01:35:15 +0100

    Añadir funciones al servidor de nombres (hasta el video 1.13)

--- cliente/WindowsFormsApplication1/Form1.Designer.cs
+++ cliente/WindowsFormsApplication1/Form1.Designer.cs
@@ -30,10 +30,15 @@
         {
             this.label2 = new System.Windows.Forms.Label();
             this.nombre = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.SendButton = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.Longitud = new System.Windows.Forms.RadioButton();
             this.Bonito = new System.Windows.Forms.RadioButton();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.DisconnectButton = new System.Windows.Forms.Button();
+            this.AlturaTextBox = new System.Windows.Forms.TextBox();
+            this.AlturaRadioButton = new System.Windows.Forms.RadioButton();
+            this.PalindromoRadioButton = new System.Windows.Forms.RadioButton();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -41,50 +46,60 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(23, 25);
+            this.label2.Location = new System.Drawing.Point(31, 31);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(87, 25);
+            this.label2.Size = new System.Drawing.Size(110, 31);
             this.label2.TabIndex = 1;
             this.label2.Text = "Nombre";
             // 
             // nombre
             // 
-            this.nombre.Location = new System.Drawing.Point(116, 31);
+            this.nombre.Location = new System.Drawing.Point(155, 38);
+            this.nombre.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.nombre.Name = "nombre";
-            this.nombre.Size = new System.Drawing.Size(164, 20);
+            this.nombre.Size = new System.Drawing.Size(217, 22);
             this.nombre.TabIndex = 3;
             // 
-            // button2
+            // SendButton
             // 
-            this.button2.Location = new System.Drawing.Point(130, 144);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "Enviar";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.SendButton.Location = new System.Drawing.Point(155, 221);
+            this.SendButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(100, 28);
+            this.SendButton.TabIndex = 5;
+            this.SendButton.Text = "Enviar";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
             // 
             // groupBox1
             // 
             this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.groupBox1.Controls.Add(this.PalindromoRadioButton);
+            this.groupBox1.Controls.Add(this.AlturaRadioButton);
+            this.groupBox1.Controls.Add(this.AlturaTextBox);
+            this.groupBox1.Controls.Add(this.DisconnectButton);
+            this.groupBox1.Controls.Add(this.connectButton);
             this.groupBox1.Controls.Add(this.Longitud);
             this.groupBox1.Controls.Add(this.Bonito);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.SendButton);
             this.groupBox1.Controls.Add(this.nombre);
-            this.groupBox1.Location = new System.Drawing.Point(12, 114);
+            this.groupBox1.Location = new System.Drawing.Point(16, 140);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(363, 282);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(484, 347);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Peticion";
             // 
             // Longitud
             // 
             this.Longitud.AutoSize = true;
-            this.Longitud.Location = new System.Drawing.Point(116, 91);
+            this.Longitud.Location = new System.Drawing.Point(155, 112);
+            this.Longitud.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Longitud.Name = "Longitud";
-            this.Longitud.Size = new System.Drawing.Size(166, 17);
+            this.Longitud.Size = new System.Drawing.Size(220, 21);
             this.Longitud.TabIndex = 7;
             this.Longitud.TabStop = true;
             this.Longitud.Text = "Dime la longitud de mi nombre";
@@ -93,20 +108,74 @@
             // Bonito
             // 
             this.Bonito.AutoSize = true;
-            this.Bonito.Location = new System.Drawing.Point(116, 68);
+            this.Bonito.Location = new System.Drawing.Point(155, 84);
+            this.Bonito.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Bonito.Name = "Bonito";
-            this.Bonito.Size = new System.Drawing.Size(156, 17);
+            this.Bonito.Size = new System.Drawing.Size(207, 21);
             this.Bonito.TabIndex = 8;
             this.Bonito.TabStop = true;
             this.Bonito.Text = "Dime si mi nombre es bonito";
             this.Bonito.UseVisualStyleBackColor = true;
             // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(66, 301);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(96, 23);
+            this.connectButton.TabIndex = 9;
+            this.connectButton.Text = "Conectar";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // DisconnectButton
+            // 
+            this.DisconnectButton.Location = new System.Drawing.Point(221, 301);
+            this.DisconnectButton.Name = "DisconnectButton";
+            this.DisconnectButton.Size = new System.Drawing.Size(110, 23);
+            this.DisconnectButton.TabIndex = 10;
+            this.DisconnectButton.Text = "Desconectar";
+            this.DisconnectButton.UseVisualStyleBackColor = true;
+            this.DisconnectButton.Click += new System.EventHandler(this.DisconnectButton_Click);
+            // 
+            // AlturaTextBox
+            // 
+            this.AlturaTextBox.Location = new System.Drawing.Point(35, 141);
+            this.AlturaTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.AlturaTextBox.Name = "AlturaTextBox";
+            this.AlturaTextBox.Size = new System.Drawing.Size(106, 22);
+            this.AlturaTextBox.TabIndex = 11;
+            // 
+            // AlturaRadioButton
+            // 
+            this.AlturaRadioButton.AutoSize = true;
+            this.AlturaRadioButton.Location = new System.Drawing.Point(155, 141);
+            this.AlturaRadioButton.Margin = new System.Windows.Forms.Padding(4);
+            this.AlturaRadioButton.Name = "AlturaRadioButton";
+            this.AlturaRadioButton.Size = new System.Drawing.Size(128, 21);
+            this.AlturaRadioButton.TabIndex = 12;
+            this.AlturaRadioButton.TabStop = true;
+            this.AlturaRadioButton.Text = "Dime si soy alto";
+            this.AlturaRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // PalindromoRadioButton
+            // 
+            this.PalindromoRadioButton.AutoSize = true;
+            this.PalindromoRadioButton.Location = new System.Drawing.Point(155, 170);
+            this.PalindromoRadioButton.Margin = new System.Windows.Forms.Padding(4);
+            this.PalindromoRadioButton.Name = "PalindromoRadioButton";
+            this.PalindromoRadioButton.Size = new System.Drawing.Size(238, 21);
+            this.PalindromoRadioButton.TabIndex = 13;
+            this.PalindromoRadioButton.TabStop = true;
+            this.PalindromoRadioButton.Text = "Dime si mi nombre es palíndromo";
+            this.PalindromoRadioButton.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(741, 562);
+            this.ClientSize = new System.Drawing.Size(988, 692);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -120,10 +189,15 @@
 
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox nombre;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button SendButton;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.RadioButton Longitud;
         private System.Windows.Forms.RadioButton Bonito;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Button DisconnectButton;
+        private System.Windows.Forms.RadioButton AlturaRadioButton;
+        private System.Windows.Forms.TextBox AlturaTextBox;
+        private System.Windows.Forms.RadioButton PalindromoRadioButton;
     }
 }
 
--- cliente/WindowsFormsApplication1/Form1.cs
+++ cliente/WindowsFormsApplication1/Form1.cs
@@ -26,13 +26,95 @@ namespace WindowsFormsApplication1
         }
 
    
-        private void button2_Click(object sender, EventArgs e)
+        private void SendButton_Click(object sender, EventArgs e)
         {
+            if (Longitud.Checked)
+            {
+                // Quiere saber la longitud
+                string mensaje = "1/" + nombre.Text;
+                // Enviamos al servidor el nombre tecleado
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                server.Send(msg);
 
+                //Recibimos la respuesta del servidor
+                byte[] msg2 = new byte[80];
+                server.Receive(msg2);
+                mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+                MessageBox.Show("La longitud de tu nombre es: " + mensaje);
+            }
+            else if (Bonito.Checked)
+            {
+                // Quiere saber si el nombre es bonito
+                string mensaje = "2/" + nombre.Text;
+                // Enviamos al servidor el nombre tecleado
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                server.Send(msg);
+
+                //Recibimos la respuesta del servidor
+                byte[] msg2 = new byte[80];
+                server.Receive(msg2);
+                mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+
+                if (mensaje == "SI")
+                    MessageBox.Show("Tu nombre ES bonito.");
+                else
+                    MessageBox.Show("Tu nombre NO bonito. Lo siento.");
+            }
+            else if (AlturaRadioButton.Checked)
+            {
+                float altura;
+                try
+                {
+                    altura = Convert.ToSingle(AlturaTextBox.Text);
+                }
+                catch(FormatException)
+                {
+                    MessageBox.Show("Error: Altura no reconocida");
+                    return;
+                }
+                string mensaje = "3/" + nombre.Text + "/" + altura;
+                // Enviamos al servidor el nombre tecleado y la altura
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                server.Send(msg);
+
+                //Recibimos la respuesta del servidor
+                byte[] msg2 = new byte[80];
+                server.Receive(msg2);
+                mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+                MessageBox.Show(mensaje);
+            }
+            else
+            {
+                // petición para ver si el nombre es palíndromo
+                string mensaje = "4/" + nombre.Text;
+                // Enviamos al servidor el nombre tecleado
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                server.Send(msg);
+
+                //Recibimos la respuesta del servidor
+                byte[] msg2 = new byte[80];
+                server.Receive(msg2);
+                string respuesta = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+                string[] respuestaSplit = respuesta.Split('/');
+                string frase = respuestaSplit[1];
+                if(Convert.ToUInt16(respuestaSplit[0]) == 1)
+                {
+                    frase  = frase + " es palindromo";
+                }
+                else
+                {
+                    frase = frase + " no es palindromo";
+                }
+                MessageBox.Show(frase);
+            }
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
             //Creamos un IPEndPoint con el ip del servidor y puerto del servidor 
             //al que deseamos conectarnos
-            IPAddress direc = IPAddress.Parse("192.168.56.102");
-            IPEndPoint ipep = new IPEndPoint(direc, 9070);
+            IPAddress direc = IPAddress.Parse("192.168.56.103");
+            IPEndPoint ipep = new IPEndPoint(direc, 9092);
 
 
             //Creamos el socket 
@@ -41,70 +123,28 @@ namespace WindowsFormsApplication1
             {
                 server.Connect(ipep);//Intentamos conectar el socket
                 this.BackColor = Color.Green;
-             
-
-                if (Longitud.Checked)
-                {
-                    // Quiere saber la longitud
-                    string mensaje = "1/" + nombre.Text;
-                    // Enviamos al servidor el nombre tecleado
-                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
-                    server.Send(msg);
-
-                    //Recibimos la respuesta del servidor
-                    byte[] msg2 = new byte[80];
-                    server.Receive(msg2);
-                    mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
-                    MessageBox.Show("La longitud de tu nombre es: " + mensaje);
-                }
-                else
-                {
-                    // Quiere saber si el nombre es bonito
-                    string mensaje = "2/" + nombre.Text;
-                    // Enviamos al servidor el nombre tecleado
-                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
-                    server.Send(msg);
-
-                    //Recibimos la respuesta del servidor
-                    byte[] msg2 = new byte[80];
-                    server.Receive(msg2);
-                    mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
-
-
-                    if (mensaje == "SI")
-                        MessageBox.Show("Tu nombre ES bonito.");
-                    else
-                        MessageBox.Show("Tu nombre NO bonito. Lo siento.");
-
-
-                }
-             
-                // Se terminó el servicio. 
-                // Nos desconectamos
-                this.BackColor = Color.Gray;
-                server.Shutdown(SocketShutdown.Both);
-                server.Close();
-
-
+                MessageBox.Show("Conectado!");
 
             }
-            catch (SocketException )
+            catch (SocketException)
             {
                 //Si hay excepcion imprimimos error y salimos del programa con return 
                 MessageBox.Show("No he podido conectar con el servidor");
                 return;
-            } 
-
-          
-
-    
-          
-          
-
+            }
         }
 
-   
-
-     
+        private void DisconnectButton_Click(object sender, EventArgs e)
+        {
+            // Nos desconectamos
+            string mensaje = "/0";
+            // Enviamos al servidor el nombre tecleado
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+            server.Send(msg);
+            this.BackColor = Color.Gray;
+            server.Shutdown(SocketShutdown.Both);
+            server.Close();
+            MessageBox.Show("Desconectado");
+        }
     }
 }
